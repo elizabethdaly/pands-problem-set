f8bd48abc511275aa2dd1cf7eb83e6863547505b
Elizabeth Daly <liz.m.a.daly@gmail.com>
2019-02-10 16:44:38 +0000

    First go at problem 1

--- problem1.py
+++ problem1.py
@@ -1,3 +1,7 @@
+# Elizabeth Daly
 # HDip Data Analytics 2019 pands problem set
+# 
 # problem1.py
-# change file location
+# Ask the user to input any positve integer
+# Output the sum of all numbers between one and that number
+#
