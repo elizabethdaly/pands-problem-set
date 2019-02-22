30af15200501db1564e1df742c67a10d8d0bf228
Elizabeth Daly <liz.m.a.daly@gmail.com>
2019-02-22 11:34:33 +0000

    Template for Problem 3

---
+++ problem3.py
@@ -0,0 +1,11 @@
+# Elizabeth Daly
+# HDip Data Analytics 2019 pands-problem-set
+# 
+# problem3.py
+#
+# Program to print all numbers between 1,000 and 10,000 that
+# are divisible by 6 but not by 12.
+# ###########################################################
+#
+# Describe the program to the user.
+print("Program to print all numbers between 1,000 and 10,000 that are divisible by 6 but not by 12.")
