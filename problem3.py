df72d57986081761e2f201f40fe45f0ed412a906
Elizabeth Daly <liz.m.a.daly@gmail.com>
2019-02-22 14:26:30 +0000

    Problem 3 done in a slightly differnt way

--- problem3.py
+++ problem3.py
@@ -20,19 +20,56 @@ ne = 10000  # end number
 # Check that first and last numbers are correct
 nums = list(range(ns,ne + 1))
 lnum = len(nums)
-print("The list to check has", len(nums), "elements from.", nums[0], "to", nums[lnum-1])
+print("The list to check has", len(nums), "elements from", nums[0], "to", nums[lnum-1])
 
-# Create an empty list to hold the answer.
-ans = []
-# If both conditions are true (/6 AND not /12) put number in new list)
+# Realised that another way to do this would be to cycle through all the numbers from 1000 to 10000
+# and generate a new list containing only those divisible by 6.
+# The second condition (is number divisible by 12) could then be checked on this shorter list.
+# ? more efficient as checking condition1 on a long list but condition2 on a much shorter one.
+
+# Create an empty list to store the numbers divisible by 6.
+ans6 = []
+
+# Generate that list.
 for x in nums:
-    if (x % 6 == 0) and (x % 12 != 0):
-        # print(x, "divisible by 6 but not by 12")
-        ans.append(x)
+    if x % 6 == 0:
+        ans6.append(x)
     else:
         continue
 
-lans = len(ans)
-print(lans, "numbers are divisible by 6 but not by 12. They are:")
-print(ans)
-# print(*ans, sep = "\n") # Print each list element on a new line.
+print(len(ans6), "numbers are divisible by 6.")
+
+# Create another empty list to store the numbers divisible by 6 but not by 12.
+ans6not12 = []
+
+# Generate that list.
+for y in ans6:
+    if y % 12 != 0:
+        ans6not12.append(y)
+    else:
+        continue
+print(len(ans6not12), "of these are also not divisible by 12.")
+
+print("These are:", ans6not12)
+
+# ######################################
+# #
+# # Original solution that works fine.
+# #
+# ######################################
+# # Create an empty list to hold the answer.
+# ans = []
+# # If both conditions are true (/6 AND not /12) put number in new list)
+# for x in nums:
+#     if (x % 6 == 0) and (x % 12 != 0):
+#         # print(x, "divisible by 6 but not by 12")
+#         ans.append(x)
+#     else:
+#         continue
+
+# lans = len(ans)
+# print(lans, "numbers are divisible by 6 but not by 12. They are:")
+# print(ans)
+# # print(*ans, sep = "\n") # Print each list element on a new line.
+
+
