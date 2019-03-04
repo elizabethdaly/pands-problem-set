eb7111da3089ef229263fbeca89c1037c9fc3910
Elizabeth Daly <liz.m.a.daly@gmail.com>
2019-03-04 20:53:05 +0000

    Problem 8 with some formatting done

--- problem8.py
+++ problem8.py
@@ -18,12 +18,16 @@ import datetime as dt
 # d = dt.datetime.today()    # also works
 d = dt.datetime.now()
 print(d)
-print(dt.datetime.strftime(d,"%A, %B %d %Y at %I:%M%p"))
 
-# strftime options
+# Now try to format it
+# Following has no suffix on day, zero pad on hr, PM vs pm. 
+print(dt.datetime.strftime(d,"%A, %B%e %Y at %I:%M%p")) 
+
+# strftime options used:
 # %A Weekday as full name
 # %B Month as full name
-# %d Day of month as zero padded decimal
+# %d Day of month as zero-padded decimal
+# %e Day of month NO zero-padding
 # %Y Year with century
-# %I Hour as zero padded decimal(12 hour clock)
-# %p am/pm
+# %I Hour as zero-padded decimal(12 hour clock)
+# %p AM/PM
