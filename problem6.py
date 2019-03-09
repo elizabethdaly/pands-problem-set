756e9e0e7c3042878c8549b1da037c5ca56e2ad8
Elizabeth Daly <liz.m.a.daly@gmail.com>
2019-03-09 18:47:19 +0000

    Start problem 6

---
+++ problem6.py
@@ -0,0 +1,18 @@
+# Elizabeth Daly
+# HDip Data Analytics 2019 pands-problem-set
+# 
+# problem6.py
+#
+# Ask the user to input any string.
+# Output every second word.
+#
+# ###########################################################
+#
+# Describe the program to the user.
+print("Program to print every second word of a sentence.")
+
+# Request the input.
+s = input("Please enter a short sentence: ")
+print(s, type(s))
+
+# s.split()
