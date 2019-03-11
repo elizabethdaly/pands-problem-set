7ad346fa17daaba372d42ebb29146fb23c3e1e54
Elizabeth Daly <liz.m.a.daly@gmail.com>
2019-03-11 18:56:46 +0000

    More work on formatting

--- problem8.py
+++ problem8.py
@@ -14,19 +14,10 @@ print("Program to output today's date in a particular format.")
 # Import the Python datetime module.
 import datetime as dt
 
-# Just get the date & time first
-d = dt.datetime.now()
-# print(type(d))
-print(d)
-
-# Extract the day of month to generate a suffix
-dom = dt.datetime.now().day
-
-# dom = 11 # For testing.
-print(dom, type(dom))
-
-
+#############################################################
+# Define a function to generate the suffix for day of month.
 def datesuffix(n):
+    """Given day of month return English Ordinal Suffix"""
     if (n == 1 or n == 21 or n == 31):
         suffix = "st"
     elif (n == 2 or n == 22):
@@ -35,14 +26,21 @@ def datesuffix(n):
         suffix = "rd"
     else:
         suffix = "th"
+    return suffix
 
-print(suffix)
+# # Test that function works correctly.
+# i = 1
+# while i < 32:
+#     print(i, datesuffix(i))
+#     i = i + 1
+#############################################################
 
+# Just get the date & time first
+d = dt.datetime.now()
+print(d, "type=", type(d))
 
-# Now try to format it
+# Now try to format it - not quite right.
 # Following has no suffix on day, zero pad on hr, PM vs pm. 
-print(dt.datetime.strftime(d,"%A, %B %e %Y at %I:%M%p")) 
-
 # strftime options used:
 # %A Weekday as full name
 # %B Month as full name
@@ -52,4 +50,24 @@ print(dt.datetime.strftime(d,"%A, %B %e %Y at %I:%M%p"))
 # %I Hour as zero-padded decimal(12 hour clock)
 # %M Minutes
 # %p AM/PM
+print(dt.datetime.strftime(d,"%A, %B %e %Y at %I:%M%p"))
+
+# Extract the day of month to generate a suffix
+dom = dt.datetime.now().day
+suf = datesuffix(dom)
+print(dom,suf)
+
+# Extract hour to generate am/pm lowercase & 12 hour clock.
+hr = dt.datetime.now().hour
+
+if hr < 12:
+    a = "am"
+else:
+    a = "pm"
+    hr = hr - 12
+print(hr,a)
+
+# Think I'll have to separate date into parts for correct formatting.
+print(dt.datetime.strftime(d,"%A, %B %e"), suf, dt.datetime.strftime(d,"%Y at"))
+print(dt.datetime.strftime(d,"%I:%M"), a)
 
