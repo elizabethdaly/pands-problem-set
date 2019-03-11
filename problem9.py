cb7d2dd60e90d6213483f4b7bce7ea1fcb96df4f
Elizabeth Daly <liz.m.a.daly@gmail.com>
2019-03-11 14:49:00 +0000

    Problem 9 now printing every second line from file

--- problem9.py
+++ problem9.py
@@ -11,29 +11,31 @@
 # Describe the program to the user.
 print("Program to print every second line from a txt file.")
 
-# sys to get access to command line arguments.
-# argv[0] is python script
-# argv[1] is file name
+# sys module to get access to command line arguments.
+# argv[0] is the Python script [problem9.py]
+# argv[1] is the file name [heaney-poem.txt]
+# To run type: python problem9.py heaney-poem.txt
 import sys
 
-# Use i to keep track of line number, want the odd lines.
+# Use i to keep track of line number, want the odd lines only.
 i = 1
+# Use with to ensure file is closed when you are finished with it.
 with open(sys.argv[1], 'r') as f:
     for line in f:
         if i % 2 == 1:
             print("Line", i, ": ", line, end='')
         i = i + 1
-        #Data = f.read()
-#print(data)
 
-# # fileimport to take filename from command line.
+# First attempts below
+#
+# # fileimport module to take filename(s) from command line.
 # # Open the file in read mode and print every line.
 # import fileinput
 # with fileinput.input(mode = 'r') as f:
 #     for line in f:
 #         print(line, end='')
 
-# # Try opening file from code first and print every line.
+# # Try opening file from within the script first and print every line.
 # with open('heaney-poem.txt', 'r') as f:
 #     for l in f:
 #         print(l)
