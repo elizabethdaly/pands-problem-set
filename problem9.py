1d2959daa73c09254651cf68d2f321a10e32823e
Elizabeth Daly <liz.m.a.daly@gmail.com>
2019-03-11 13:01:22 +0000

    Trying a few things

--- problem9.py
+++ problem9.py
@@ -3,13 +3,32 @@
 # 
 # problem9.py
 #
-# Ask the user to input a file name.
-# Print every second line.
-#
+# Program to read a text file and print every second line.
+# The filename is taken from an argument on the command line.
+# File is: heaney-poem.txt
 # ###########################################################
 #
 # Describe the program to the user.
 print("Program to print every second line from a txt file.")
 
-# Request the input.
-s = input("Please enter the full file name: ")
+# fileimport to take filename from command line.
+# Open the file in read mode.
+import fileinput
+with fileinput.input(mode = 'r') as f:
+    for line in f:
+        print(line, end='')
+
+
+
+# # Use fileinput module to 
+# import fileinput
+# f = fileinput.input()
+# for line in f:
+#     print(line)
+
+
+
+# # Try opening file from code first and print every line.
+# with open('heaney-poem.txt', 'r') as f:
+#     for l in f:
+#         print(l)
