2d6bc01bdf54c9bc9a826be496a331b9219124f8
Elizabeth Daly <liz.m.a.daly@gmail.com>
2019-03-11 11:59:46 +0000

    Start problem 9

---
+++ problem9.py
@@ -0,0 +1,15 @@
+# Elizabeth Daly
+# HDip Data Analytics 2019 pands-problem-set
+# 
+# problem9.py
+#
+# Ask the user to input a file name.
+# Print every second line.
+#
+# ###########################################################
+#
+# Describe the program to the user.
+print("Program to print every second line from a txt file.")
+
+# Request the input.
+s = input("Please enter the full file name: ")
