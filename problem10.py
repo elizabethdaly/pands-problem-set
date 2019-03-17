54ba811b03ce78493ec7caa459700d01a4565fd4
elizabethdaly <liz.m.a.daly@gmail.com>
2019-03-17 15:35:48 +0000

    Get set up for problem 10

---
+++ problem10.py
@@ -0,0 +1,14 @@
+# Elizabeth Daly
+# HDip Data Analytics 2019 pands-problem-set
+# 
+# problem10.py
+#
+# Plot the functions x, x^2, and 2^x in the range [0, 4].
+#
+# ###########################################################
+#
+# Describe the program to the user.
+print("Program to plot some functions.")
+
+# Import the Python datetime module.
+import datetime as dt
