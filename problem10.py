3d8726f3181d03ac5780606f7c2cdaf3783e805d
elizabethdaly <liz.m.a.daly@gmail.com>
2019-03-17 16:01:33 +0000

    Begin problem 10 properly

--- problem10.py
+++ problem10.py
@@ -10,5 +10,26 @@
 # Describe the program to the user.
 print("Program to plot some functions.")
 
-# Import the Python datetime module.
-import datetime as dt
+# Import matplotlib.
+import matplotlib.pyplot as plt
+
+# Import numpy for computation.
+import numpy as np
+
+# Generate the list of x values.
+xvals = list(range(5))
+print("x values are: ", xvals)
+
+# Generate list of x squared values.
+xsq = [i**2 for i in xvals]
+print("x squared is", xsq)
+
+# Get started. Prompt doesn't come back until figure window closed.
+plt.plot(xvals)
+plt.ylabel('y')
+plt.xlabel('x')
+plt.show()
+
+# will need exp2 for 2**p with p input array
+# might need to save plots/ plot if all on one figure
+# annotate [plots x, x^2, 2^x]
